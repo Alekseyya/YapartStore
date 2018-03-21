8f178f7e21de77cd5706bd426b8accf779753138
Fokiruna <alekseyyarchuk@gmail.com>
2018-03-21 20:23:27 +0300

    First arcitecture

---
+++ YapartStore/YapartStore.API/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace YapartStore.API.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
---
+++ YapartStore/YapartStore.API/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="YapartStore.API.WebApiApplication" Language="C#" %>
