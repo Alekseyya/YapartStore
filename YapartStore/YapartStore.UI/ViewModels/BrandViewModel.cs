8ddc618d902e88c2453b5876ae2941a7ead23350
Fokiruna <alekseyyarchuk@gmail.com>
2018-04-11 17:54:23 +0300

    Correct layout

---
+++ YapartStore/YapartStore.UI/ViewModels/BrandViewModel.cs
@@ -0,0 +1,11 @@
+﻿
+using System.Collections.Generic;
+
+namespace YapartStore.UI.ViewModels
+{
+    public class BrandViewModel
+    {
+        public string Name { get; set; }
+        public ICollection<ProductViewModel> Products { get; set; }
+    }
+}
