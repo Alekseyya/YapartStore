71a3579ba42051dfcb2725424ed49daec1850c73
Fokiruna <alekseyyarchuk@gmail.com>
2018-07-20 13:57:55 +0300

    Add select cars

--- YapartStore/YapartStore.UI/Scripts/JS/myScript.js
+++ YapartStore/YapartStore.UI/Scripts/JS/myScript.js
@@ -1,30 +1,65 @@
 ﻿'use strict';
 
-let liArray = document.getElementById("enum").children;
+let liArray = document.getElementById("select-car__enum").children;
+let tmpList = ["Audi", "Acura", "BMW", "Caddilac", "Changan", "Cherry", "Citroen", "Chevrolet"];
 
-for (var i = 0 ; i < liArray.length; i++) {
+function AddEventListner(){
+    for (var i = 0; i < liArray.length; i++) {
 
-    liArray[i].addEventListener('click', function(event) {
-        SortList(event.target.innerHTML);
-       // over here you can set the content in main div
-    });
-
+        liArray[i].addEventListener('click', function (event) {
+            CreateLinkListCars(event.target.innerHTML, "select-car__list");            
+        });    
+    }    
 }
 
-function SortList(text){
-    let parts = document.getElementsByClassName("parts")[0].children;
-    for (let part of parts) {
-       if(part.innerHTML.charAt(0) === text){
-           part.setAttribute("style", "color:red; display: inline;");
-       }else{
-          part.setAttribute("style", "display: none");
-       }      
+function DeleteSelectedList(){
+    let remoteDiv = document.getElementsByClassName("select-car__selected")[0];
+    if (remoteDiv) {
+        remoteDiv.parentNode.removeChild(remoteDiv);
+        return true;
+    }
+    return false;
+}
+
+
+function CreateLinkListCars(carName, newClassName) {
+    if (tmpList.length != 0) {
+        DeleteSelectedList();
+
+        let carsNameArray = FilterCarsList(carName);
+
+        //create div
+        let divCarsList = document.createElement("div");
+        divCarsList.className = "select-car__selected";
+        document.getElementsByClassName(newClassName)[0].appendChild(divCarsList);
+
+        for (let carName of carsNameArray) {
+            let aTag = document.createElement("a");
+            aTag.style = "display:inline";
+            aTag.innerHTML = carName;
+            aTag.href = "#";
+            divCarsList.appendChild(aTag);
+        }
     }
 }
 
-function CreateGroupEnum(listEnums){
-
+function FilterCarsList(carName){
+    let carsNameArray = [];
+        for (let car of tmpList) {
+            if (car.charAt(0) === carName) {
+                carsNameArray.push(car);
+            }
+        }
+    return carsNameArray;
 }
+
+
+(function(){
+    AddEventListner();       
+}())
+
+
+
 
 // (function () {
 //     let liArray = document.getElementById("temp").children;
