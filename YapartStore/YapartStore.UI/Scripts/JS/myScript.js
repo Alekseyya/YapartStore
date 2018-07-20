937ed57455b3d91ae6043d0c51b6d87565965ed0
Fokiruna <alekseyyarchuk@gmail.com>
2018-07-20 16:10:02 +0300

    Correct show all cars is selected list

--- YapartStore/YapartStore.UI/Scripts/JS/myScript.js
+++ YapartStore/YapartStore.UI/Scripts/JS/myScript.js
@@ -3,17 +3,47 @@
 let liArray = document.getElementById("select-car__enum").children;
 let tmpList = ["Audi", "Acura", "BMW", "Caddilac", "Changan", "Cherry", "Citroen", "Chevrolet"];
 
-function AddEventListner(){
+let popularList = ["Audi", "Acura", "BMW", "Caddilac", "Changan"];
+
+let createClassName = "select-car__selected";
+let appendClassName = "select-car__list";
+
+function AddEventListner() {
+    //for char cars
     for (var i = 0; i < liArray.length; i++) {
 
         liArray[i].addEventListener('click', function (event) {
-            CreateLinkListCars(event.target.innerHTML, "select-car__list");            
+            CreateCarList(event);
+            ActiveSelectChar(event.target);
         });    
-    }    
+    } 
+    //view all or popular
+    document.querySelector('.select-car__buttons').addEventListener('click',
+        function(event) {
+            ShowPopulerOrNotCars(event.target);
+        });
 }
 
-function DeleteSelectedList(){
-    let remoteDiv = document.getElementsByClassName("select-car__selected")[0];
+function ShowPopularCars() {
+    CreateDivCarList(createClassName, appendClassName, popularList);
+}
+
+function ShowPopulerOrNotCars(span) {
+    if (span.className === "select-car__show-all") {
+        ResetSelection();
+        span.style = "display: none";
+        CreateDivCarList(createClassName, appendClassName, tmpList);
+        document.querySelector('.select-car__show-popular').style = "display: initial";
+    } else {
+        span.style = "display: none";
+        ResetSelection();
+        CreateDivCarList(createClassName, appendClassName, popularList);
+        document.querySelector('.select-car__show-all').style = "display: initial";
+    }
+}
+
+function DeleteSelectedList(className){
+    let remoteDiv = document.getElementsByClassName(className)[0];
     if (remoteDiv) {
         remoteDiv.parentNode.removeChild(remoteDiv);
         return true;
@@ -22,41 +52,62 @@ function DeleteSelectedList(){
 }
 
 
-function CreateLinkListCars(carName, newClassName) {
+function CreateCarList(event) {
+    let charCar = event.target.innerHTML;
     if (tmpList.length != 0) {
-        DeleteSelectedList();
-
-        let carsNameArray = FilterCarsList(carName);
-
-        //create div
-        let divCarsList = document.createElement("div");
-        divCarsList.className = "select-car__selected";
-        document.getElementsByClassName(newClassName)[0].appendChild(divCarsList);
-
-        for (let carName of carsNameArray) {
-            let aTag = document.createElement("a");
-            aTag.style = "display:inline";
-            aTag.innerHTML = carName;
-            aTag.href = "#";
-            divCarsList.appendChild(aTag);
+        if (event.target.id === "all-cars") {
+            CreateDivCarList(createClassName, appendClassName, tmpList);
+        } else {
+            let carsNameArray = FilterCarsList(charCar);
+            CreateDivCarList(createClassName, appendClassName, carsNameArray);
         }
     }
 }
 
-function FilterCarsList(carName){
+function ActiveSelectChar(tag) {
+    ResetSelection();
+    tag.className = "active";
+}
+
+function ResetSelection() {
+    let liArray = document.querySelectorAll('#select-car__enum li:not(.all-cars)');
+    for (let li of liArray) {
+        li.className = "";
+    }
+}
+
+function CreateDivCarList(className, appendClassName, carsArray) {
+    DeleteSelectedList("select-car__selected");
+
+    let divCarsList = document.createElement("div");
+    divCarsList.className = className;
+    document.getElementsByClassName(appendClassName)[0].appendChild(divCarsList);
+
+    for (let carName of carsArray) {
+        let aTag = document.createElement("a");
+        aTag.style = "display:inline";
+        aTag.innerHTML = carName;
+        aTag.href = "#";
+        divCarsList.appendChild(aTag);
+    }
+}
+
+function FilterCarsList(charCar){
     let carsNameArray = [];
-        for (let car of tmpList) {
-            if (car.charAt(0) === carName) {
-                carsNameArray.push(car);
+        for (let carName of tmpList) {
+            if (carName.charAt(0) === charCar) {
+                carsNameArray.push(carName);
             }
         }
     return carsNameArray;
 }
 
 
-(function(){
-    AddEventListner();       
-}())
+
+(function() {
+    AddEventListner();
+    ShowPopularCars();
+}());
 
 
 
